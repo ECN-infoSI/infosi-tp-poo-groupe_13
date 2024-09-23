32aecfb10c77cf3603005b8cffba79e07e911f8b
Nadhem031 <Nadhem.Ben-Wahida@eleves.ec-nantes.fr>
2024-09-23 14:59:24 +0200

    first commit

---
+++ mavenproject1/src/main/java/edu/centralenantes/mavenproject1/Lapin.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.centralenantes.mavenproject1;
+
+/**
+ *
+ * @author Nadhem
+ */
+public class Lapin extends Monstre {
+
+    /**
+     *
+     * @param pV
+     * @param dA
+     * @param pPar
+     * @param paAtt
+     * @param paPar
+     * @param p
+     */
+    public Lapin(int pV, int dA, int pPar, int paAtt, int paPar, Point2D p){
+        super(pV,dA,pPar,paAtt,paPar,p);
+    }
+
+    /**
+     *
+     * @param l
+     */
+    public Lapin(Lapin l){
+        super(l);
+    }
+
+    /**
+     *
+     */
+    public Lapin(){
+        super();
+    }
+}
